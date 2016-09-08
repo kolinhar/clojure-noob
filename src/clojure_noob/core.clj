5b2d259706571b37e4c0a538b4ed3cab30605182
Remy <ext.modis.remy.juanes@sncf.fr>
2016-09-08 15:57:35 +0200

    first commit

---
+++ src/clojure_noob/core.clj
@@ -0,0 +1,3 @@
+(ns clojure-noob.core)
+;COMMENTAIRE
+(fn [] "Le fonctionnel, c'est la vie!")
---
+++ src/clojure_noob/untitled-1.clj
@@ -0,0 +1,12 @@
+(ns clojurePourLesNazes)
+;COMMENTAIRE
+((fn [] "Le fonctionnel, c'est la vie!"))
+
+;contuenu original de core.clj
+(ns clojure-noob.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
